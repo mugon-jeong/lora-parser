1d8862be9d0e176a91b375090e4de6e3552bf417
conny <conny@musma.net>
2024-12-20 09:43:18 +0900

    주석 추가

--- src/main/kotlin/io/parser/lora/LoraParsable.kt
+++ src/main/kotlin/io/parser/lora/LoraParsable.kt
@@ -80,9 +80,48 @@ interface LoraParsable {
         }
 
         /**
-         * 리플렉션을 사용해 클래스 [T]의 인스턴스를 랜덤 값으로 생성하는 함수
-         * @param devEUI 장치 고유 식별자 (선택적)
-         * @return 랜덤 [T] 객체
+         * 리플렉션을 사용하여 클래스 [T]의 인스턴스를 랜덤 값으로 생성하는 함수.
+         *
+         * 이 함수는 주어진 `providers` 맵을 사용하여 특정 필드에 대해 커스텀 랜덤 값을 제공하거나,
+         * 클래스 기반 랜덤 값 생성 로직(`classBasedRandomProvider`) 및 어노테이션 기반 처리(예: `@ParseStatus`)를 통해
+         * 자동으로 값을 생성합니다.
+         *
+         * @param T 랜덤 값을 생성할 클래스의 타입. 반드시 `LoraParsable`을 구현해야 합니다.
+         * @param devEUI 장치 고유 식별자
+         * @param providers 필드 이름을 키로 하고 해당 필드의 랜덤 값을 생성할 [RandomProvider]를 매핑한 맵.
+         *                  특정 필드에 대한 값을 커스터마이징하려면 이 맵에 프로바이더를 등록하십시오.
+         * @param classBasedRandomProvider 특정 클래스 타입([KClass<*>])에 대해 커스텀 랜덤 값을 생성하는 람다 함수.
+         *                                 기본값은 `null`을 반환하며, 값이 없을 경우 기본 핸들러를 사용합니다.
+         * @return 랜덤 [T] 객체.
+         *
+         * @throws IllegalArgumentException 다음과 같은 경우 예외를 발생시킵니다:
+         * - 클래스에 기본 생성자가 없거나, 생성자 파라미터가 필드와 매핑되지 않는 경우.
+         * - 지원되지 않는 어노테이션 또는 필드가 있는 경우.
+         *
+         * 동작 방식:
+         * 1. 클래스의 모든 생성자 파라미터에 대해 다음 순서로 값을 확인합니다:
+         *    - 필드 이름 기반 프로바이더(`providers`)에서 값을 가져옵니다.
+         *    - 어노테이션(`@ParseStatus`) 기반으로 값 생성 여부를 확인합니다.
+         *    - 클래스 타입 기반 프로바이더(`classBasedRandomProvider`)를 사용하여 값을 생성합니다.
+         *    - 위의 모든 과정에서 값이 없는 경우, 기본 핸들러를 사용하여 값을 생성합니다.
+         * 2. 생성된 값들을 매핑하여 [T] 타입의 객체를 반환합니다.
+         *
+         * @sample
+         * ```kotlin
+         * val weather = LoraParsable.random<Weather>(
+         *     devEUI = "1234567890abcdef",
+         *     providers = mapOf(
+         *         "temperature" to ListBasedRandomProvider(listOf(BigDecimal("15.0"), BigDecimal("30.0"))),
+         *         "humidity" to RangeBasedRandomProvider(50..90)
+         *     ),
+         *     classBasedRandomProvider = {
+         *         when (it) {
+         *             SensorStatus::class -> SensorStatus.random() // SensorStatus에 대해 커스텀 랜덤 생성기 사용
+         *             else -> null
+         *         }
+         *     }
+         * )
+         * ```
          */
         inline fun <reified T : LoraParsable> random(
             devEUI: String,
